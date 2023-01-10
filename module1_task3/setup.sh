0e11078e354afb29d34f320b66538f54787e4f91
Jehp00 <jehp01104006@gmail.com>
2023-01-10 18:27:08 -0500

    task 3

--- module1_task3/setup.sh
+++ module1_task3/setup.sh
@@ -4,6 +4,6 @@
 
 #* The production website is hosted on Ubuntu 18.04 Docker Conteiner
 #* The applications "GoHugo" and "Make" are installed with `apt-get update && apt-get install -y hugo make`.
-sudo apt-get update && sudo apt-get install -y hugo make
+apt-get update && apt-get install -y hugo make
 # Put your root password and when runs the command make build, there is a buch of errors that ends in the follow line
 make build
