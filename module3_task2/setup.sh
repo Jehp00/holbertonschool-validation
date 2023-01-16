ea043c3c06950845a24de35dac9cebf3353b41eb
Jehp00 <jehp01104006@gmail.com>
2023-01-15 20:24:12 -0500

    tasks

--- module3_task2/setup.sh
+++ module3_task2/setup.sh
@@ -1,6 +1,14 @@
-apt-get update && apt-get install -y curl make zip
-npm install -g markdownlint-cli
+#!/bin/bash
 
-curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo_extended.deb
-apt-get install -y ./hugo_extended.deb
-rm hugo_extended.deb
+## Production Instructions
+
+#* The production website is hosted in an Ubuntu 18.04 Docker container
+#* The applications "GoHugo" and "Make" are installed with `apt-get update && apt-get install -y hugo make`.
+apt update
+apt install -y git
+apt install -y hugo
+apt update
+apt-get update && apt-get install make
+apt install -y curl
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
+apt install ./hugo.deb
